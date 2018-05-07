a4f0f8d32bd79d43c6965cf7fb8918136b4473e4
DanielaAduviri <danian0225@gmail.com>
2018-05-07 16:44:21 -0500

    cambio

--- gui/src/App.js
+++ gui/src/App.js
@@ -6,26 +6,21 @@ import Portada from './components/Portada/Portada';
 import Login from './components/Login/Login';
 import Categoria from './components/Categoria/Categoria';
 import Perfil from './components/Perfil/Perfil';
-<<<<<<< HEAD
 import Registrar from './components/Registrar/Registrar';
 import AnuncioPop from './components/AnuncioPop/AnuncioPop';
-
-
-=======
 import Agregar from './components/Agregar/Agregar';
->>>>>>> e9f0cdd2383cd5cf59374217b9787068b33ae5dc
 
 class App extends Component {
   render() {
     return (
       <Layout>
         {/*<Portada />*/}
-<<<<<<< HEAD
+
         
         <AnuncioPop/>
-=======
 
->>>>>>> e9f0cdd2383cd5cf59374217b9787068b33ae5dc
+
+
       </Layout>
     );
   }
