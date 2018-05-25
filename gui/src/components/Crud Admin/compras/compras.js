ac5d4be470c494ed3ca3893c228e1ae99c8151f4
Jerson Huayta <jerson.huayta@tecsup.edu.pe>
2018-05-24 20:07:30 -0500

    inputs

---
+++ gui/src/components/Crud Admin/compras/compras.js
@@ -0,0 +1,24 @@
+
+
+const plan = () => (
+	<div>
+	
+
+	<h1>Plan</h1>
+		<label  >Porcentaje:</label>
+		
+		<input  type="number" />
+
+		<label  >Tiempo:</label>
+		
+		<input  type="number" />
+
+		<label  >Precio:</label>
+		
+		<input  type="number" />
+		
+	
+	
+	</div>);
+
+export default plan;
