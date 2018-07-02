e44e769a64ba12fcc492fa8cbe3e442083d79be6
JohanJoshua <johan.miranda@tecsup.edu.pe>
2018-07-02 04:06:25 +0000

    .

--- BYC Build/app.js
+++ BYC Build/app.js
@@ -51,19 +51,5 @@ app.use('/anuncios', anuncioRoutes);
 app.use('/compras', compraRoutes);
 app.use('/comentarios', comentarioRoutes);
 
-app.use((req,res,next)=>{
-	const error = new Error('Page Not found');
-	error.status = 404;
-	next(error);
-});
-
-app.use((error,req,res,next)=>{
-	res.status(error.status || 500);
-	res.json({
-		error:{
-			message: error.message
-		}
-	});
-});
 
 module.exports = app;
